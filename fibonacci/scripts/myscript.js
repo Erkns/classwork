56d225a72d0860c6c868f8e5d24a48800ebcabda
erk <ens_sp@hotmail.com>
2015-11-13 14:21:16 -0600

    fibonacci series add

--- fibonacci/scripts/myscript.js
+++ fibonacci/scripts/myscript.js
@@ -2,19 +2,17 @@ $(document).ready(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
     var limit = parseInt($('input#number').val());
-    var input = 0;
-    for (var x = 0; x < 100; x++) {
-      if (x === limit + 1) {
-        break;
-      }
-    }
-    input = x;
-    for (var y = 0; y < 50; y + y) {
-      if (input === limit) {
-        break;
+    var fibonacciSequence = [];
+    for (var i = 0; i < limit; i++) {
+      if (i === 0 || i === 1) {
+        fibonacciSequence.push(1);
+      } else {
+        var lastTwo = fibonacciSequence.slice(-1)[0] +
+          fibonacciSequence.slice(-2)[0];
+        fibonacciSequence.push(lastTwo);
       }
     }
 
-    console.log(y);
+    console.log(fibonacciSequence);
   });
 });
